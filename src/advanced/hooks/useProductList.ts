c70a0dfcdc3cbbb786939d1399896b27b464b8bf
lunashp <hijnshp@gmail.com>
2024-10-17 13:42:34 +0900

    FEAT: lucky sale 로직 추가

--- src/advanced/hooks/useProductList.ts
+++ src/advanced/hooks/useProductList.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Product } from '../types';
 
 export const useProductList = () => {
@@ -9,6 +9,41 @@ export const useProductList = () => {
     { id: 'p4', name: '상품4', price: 15000, stock: 0 },
     { id: 'p5', name: '상품5', price: 25000, stock: 10 }
   ]);
+
+  useEffect(() => {
+    const ENTIRE_DISCOUNT_RATE = 0.2;
+    const ONE_SECOND = 1000;
+
+    // Lucky Sale
+    const luckySaleTimeout = setTimeout(
+      () => {
+        const luckySaleInterval = setInterval(() => {
+          const luckyItemIndex = Math.floor(Math.random() * productList.length);
+          const luckyItem = productList[luckyItemIndex];
+
+          if (Math.random() < 0.3 && luckyItem.stock > 0) {
+            setProductList((prevList) =>
+              prevList.map((product, index) =>
+                index === luckyItemIndex
+                  ? {
+                      ...product,
+                      price: Math.round(product.price * (1 - ENTIRE_DISCOUNT_RATE))
+                    }
+                  : product
+              )
+            );
+            alert(`번개세일! ${luckyItem.name}이(가) 20% 할인 중입니다!`);
+          }
+        }, 30 * ONE_SECOND);
+      },
+      Math.random() * 10 * ONE_SECOND
+    );
+
+    // Clean up intervals and timeouts
+    return () => {
+      clearTimeout(luckySaleTimeout);
+    };
+  }, [productList]);
 
   const decreaseStock = (productId: string, amount: number) => {
     // 상태 업데이트를 위한 함수
